aa3fed3b6282ed9783b59ea5bddadef69ae0d65b
mdtahidulislam <tahidul.developer@gmail.com>
2021-12-30 13:52:22 +0600

    added owl carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,25 @@
-// (function ($) {
-// 	"use strict";
-
-
-// })(jQuery);
+(function ($) {
+	"use strict";
+    /*================================
+        soutcebook tab carousel
+    =================================*/
+    $(document).ready(function(){
+        $(".sourcebook-active").owlCarousel({
+            margin: 15,
+            dots: false,
+            loop: true,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                600: {
+                    items: 2,
+                },
+                1000: {
+                    items: 5,
+                }
+            }
+        });
+      });
+})(jQuery);
