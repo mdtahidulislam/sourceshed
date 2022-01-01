0780c5094b64083c56b555079f5041bef7c9e9ea
mdtahidulislam <tahidul.developer@gmail.com>
2022-01-01 18:19:45 +0600

    done menu responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,26 @@
 (function ($) {
 	"use strict";
     /*================================
+        mobile menu
+    =================================*/
+    $(".header-nav__bar").click(function(){
+        $(".drawer-nav").addClass("active");
+    });
+    $(".menu-close").click(function(){
+        $(".drawer-nav").removeClass("active");
+    });
+    $(".drawer-topmenu > a").each(function(index, item) {
+        $(this).click(function(){
+            $(this).parent().toggleClass("active");
+        });
+    });
+    $(".drawer-topmenu > a").each(function(index, item){
+        $(item).click(function() {
+            $(item).next().toggle();
+        });
+    });
+
+    /*================================
         soutcebook tab carousel
     =================================*/
     $(document).ready(function(){
